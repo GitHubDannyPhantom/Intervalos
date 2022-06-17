a9772192c18258cc045ed12a4fc501353b06e32a
Daniel Hernández Vázquez <107671076+GitHubDannyPhantom@users.noreply.github.com>
2022-06-17 00:11:51 -0700

    Create Listas.js

---
+++ Listas.js
@@ -0,0 +1,19 @@
+
+
+// Creación:
+let lenguajes = ['JavaScript', 'Python', 'Java', 'PHP'];
+console.log(lenguajes.length);
+
+// Acceso:
+console.log(lenguajes[0]);
+console.log(lenguajes[1]);
+console.log(lenguajes[2]);
+console.log(lenguajes[3]);
+
+// Modificación
+lenguajes[0] = 'JavaScript ES6';
+console.log(lenguajes[0]);
+
+// Eliminación
+lenguajes.pop();
+console.log(lenguajes.length);
